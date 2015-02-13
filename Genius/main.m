10c0bfca0b0dff5c1ae03045eeea3a94ec20e3c0
Fernando Baptista <fernandohsb@gmail.com>
2015-02-13 17:31:16 -0200

    Group Name

--- Genius/main.m
+++ Genius/main.m
@@ -2,8 +2,8 @@
 //  main.m
 //  Genius
 //
-//  Created by Rafael  Hieda on 13/02/15.
-//  Copyright (c) 2015 Rafael Hieda. All rights reserved.
+//  Created by FRAV GROUP on 13/02/15.
+//  Copyright (c) 2015 FRAV GROUP. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
