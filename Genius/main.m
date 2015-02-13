0e5fb0b3b06f720455996a1e7eb7aa4cf509b6a7
Vivian Chiodo Dias <vichiodo@MacBook-Pro-de-Vivian.local>
2015-02-13 16:36:42 -0200

    yh

--- Genius/main.m
+++ Genius/main.m
@@ -12,6 +12,9 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         NSLog(@"Hello, World!");
+        
+        for(int i= 0; i < 100; i++)
+            NSLog(@" ");
     }
     return 0;
 }
